0a0973853684dde022b70c171a40f7917b82b83d
hanamDevelopr <zmf1118@gmail.com>
2024-07-03 20:47:11 +0900

    get 메서드 호출

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(':id')
+  getHello(@Req() req: Request, @Body() Body, @Param() param): string {
+    console.log("params" ,param.id)
     return this.appService.getHello();
   }
 }
