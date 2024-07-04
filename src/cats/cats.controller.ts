469e2e50bd502f826b7c1a3c43ec979d90ffd0fd
hanamDevelopr <zmf1118@gmail.com>
2024-07-04 21:36:35 +0900

    success 인터셉터 사용해보기

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,11 +9,14 @@ import {
   Post,
   Put,
   UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
@@ -28,7 +31,9 @@ export class CatsController {
   getCat(@Param('id', ParseIntPipe) param: number) {
     console.log(param)
     
-    return 'one cat';
+    return {
+      cats: 'success one cat'
+    };
   }
 
   @Post()
--- src/common/interceptors/success.interceptor.ts
+++ src/common/interceptors/success.interceptor.ts
@@ -1,17 +1,22 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class SuccessInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('Before...');
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
-      );
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data,
+      })),
+    );
   }
 }
