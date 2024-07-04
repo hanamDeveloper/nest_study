a9c846a032781e3f13b376563481dc3b09d642b1
hanamDevelopr <zmf1118@gmail.com>
2024-07-04 21:17:18 +0900

    많이 사용되는 에러 방법

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -1,6 +1,14 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 
+type ERROR =
+  | string
+  | { error: string; statusCode: number; message: string | string[] };
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
@@ -8,15 +16,21 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-    const error = exception.getResponse();
+    const error = exception.getResponse() as ERROR;
 
-    response
-      .status(status)
-      .json({
-        statusCode: status,
+    if (typeof error === 'string') {
+      response.status(status).json({
+        success: false,
         timestamp: new Date().toISOString(),
         path: request.url,
-        error: error
+        error,
       });
+    } else {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        ...error,
+      });
+    }
   }
 }
