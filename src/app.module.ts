332aa172709a9dd7ca7d02f23aaccb9ec3ba6a10
hanamDevelopr <zmf1118@gmail.com>
2024-07-09 21:43:03 +0900

    스키마 설계 및 class validator 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
+
+    mongoose.connect(process.env.MONGODB_URI);
   }
 }
