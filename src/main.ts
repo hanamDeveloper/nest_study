9db8152f0ef1a38c6f85736353dbc91ce86c8ac0
hanamDeveloper <zmf1118@gmail.com>
2024-07-16 22:23:40 +0900

    스웨거 보안 추가

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import * as expressBasicAuth from 'express-basic-auth'; // 이렇게 변경
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
